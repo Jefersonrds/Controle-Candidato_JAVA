49eae56f3369d382396c4967fadc8007a83085cd
Jefersonrds <contatojefersonrds@gmail.com>
2025-04-22 23:27:31 -0300

    Feat: Visualizar propostas dos candidados e exibir os escolhidos na tela

--- controle-candidatos/src/ControleCandidatos.java
+++ controle-candidatos/src/ControleCandidatos.java
@@ -1,24 +1,70 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ControleCandidatos {
 
     public static void main(String[] args) {
-        double salarioBase = 2000.0;
 
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Para iniciarmos, favor informar a pretencao salarial:");
-            double salarioPretendido = sc.nextDouble();
+        // analisarCandidato(1900.0);
+        // analisarCandidato(2200.0);
+        // analisarCandidato(2000.0);
 
-            
-        }catch (InputMismatchException erro){
-            System.out.println("Voce nao informou um salario numerico! Favor reenviar a pretencao salarial.");
-        }
+        selecaoCandidatos();
 
-        
-        
      }
 
+     //Selecao de candidados com base no salrio
+     static void selecaoCandidatos(){
+        String [] candidatos = {"Felipe", "Marcia", "Julia", "Paulo", "Agusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+        String [] selecionados = new String[4];
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        
+        while (candidatosSelecionados < 5 && candidatoAtual<candidatos.length){
+
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.printf("O candidato %s solicitou este valor de salario: %.2f \n",candidato, salarioPretendido);
+            
+            //Verificar se o salario base e maior que o preendido
+            if (salarioBase>salarioPretendido) {
+                System.out.println("O candidato "+candidato+" foi selecionado.");
+                
+                //Acrescenta o candidato ao vetor selecionados
+                selecionados[candidatosSelecionados] = candidato;
+                candidatosSelecionados++;
+
+            } 
+            candidatoAtual++;       
+        }
+
+        //Imprimindo candidatos na tela
+        System.out.println("Os candidatos selecionados foram: ");    
+        for(int j=0;j<=selecionados.length;j++){
+            System.out.println(selecionados[j]);
+        }
+
+     }
+
+     static double valorPretendido (){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+     }
+
+     static void analisarCandidato (double salarioPretendido){
+        double salarioBase = 2000.0;
+ 
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if (salarioBase == salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO E OFERECER CONTRA PROPOSTA");
+        }else if (salarioBase < salarioPretendido){
+            System.out.println("AGUARDNADO DEMAIS CANDIDATOS");
+        }
+
+
+     }
      
 
     
